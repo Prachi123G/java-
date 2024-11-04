2d5c8eac8595b93278709f8687d163a5f765fdcf
Prachi123G <prachighate02@gmail.com>
2024-11-04 21:50:01 +0530

    New file Added

---
+++ NumberPyramid.java
@@ -0,0 +1,25 @@
+
+    import java.util.Scanner;
+
+    public class NumberPyramid {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter the number of rows: ");
+            int rows = scanner.nextInt();
+
+            for (int i = 1; i <= rows; i++) {
+                // Print spaces
+                for (int j = i; j < rows; j++) {
+                    System.out.print(" ");
+                }
+                // Print numbers
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
